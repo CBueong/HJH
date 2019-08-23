daef6fa8e6ee1f0b1bc3caf12b3a1476f561a1b4
CBueong <54173797+CBueong@users.noreply.github.com>
2019-08-23 20:37:12 +0900

    0823_FPS7set
    startFPS6to7

---
+++ compare practice/FPS7/FPS7_HJH/Bomb.cpp
@@ -0,0 +1,108 @@
+﻿#include "pch.h"
+#include "Bomb.h"
+
+#include "GameManager.h"
+#include "Ani.h"	// add animation ,logic
+
+Bomb::Bomb(int x, int y) : Object(x, y) , m_pAni(new Ani())
+{
+	m_pAni->Resize(1);
+	m_pAni->Add(0,
+		{
+			{{"XXXXX"},
+			 {"XXXXX"},
+			 {"|   |"},
+			 {"| B |"},
+			 {"#####"}},
+		   {{"xxxxx"},
+			{"xxxxx"},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{"XXXXX"},
+			{"XXXXX"},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{"xxxxx"},
+			{"xxxxx"},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{" XXX "},
+			{" XXX "},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{" xxx "},
+			{" xxx "},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{"  X  "},
+			{"  X  "},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{"  x  "},
+			{"  x  "},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{"     "},
+			{"  x  "},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+
+		   {{"     "},
+			{"     "},
+			{"|   |"},
+			{"| B |"},
+			{"#####"}},
+		}
+	);
+	m_pAni->SetState(0);
+ 	m_pNowAni = m_pAni->Get();
+}
+
+Bomb::~Bomb() { }
+
+eObjectType Bomb::GetObjectType() const
+{
+	return eObjectType::Bomb;
+}
+
+void Bomb::Init()
+{
+	GameMng()->GetBombData(this);
+}
+
+bool Bomb::_Update(float a_fDelta)
+{
+	m_pAni->Update(a_fDelta);
+	m_pNowAni = m_pAni->Get();
+	m_fLifeTime -= a_fDelta;
+
+	if(m_fLifeTime <= 0.0f)
+	{
+	COORD c = rt.Center();
+	GameMng()->ResistExplosion(c.X,c.Y);
+	return true;
+	}
+	return false;
+}
+
+void Bomb::Explosived(Bomb* a_refBomb)
+{
+	if (a_refBomb == this) { return; }
+
+	
+}
---
+++ compare practice/FPS7/FPS7_HJH/Player.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Object.h"
+
+class Player : public Object
+{
+public:
+	Player(int x, int y);
+	virtual ~Player();
+
+	// Object을(를) 통해 상속됨
+	virtual eObjectType GetObjectType() const override;
+
+	virtual Rect GetRendertRect()const override;	//+
+	void SetStat(struct CharacterData* a_pStat);
+
+private:
+	void Move(float a_fDeltaTime);
+	void BombCheck();
+protected:
+	virtual bool _Update(float a_fDelta);
+	RenderTile	m_Data;
+struct CharacterData* m_refStat;
+public:
+int m_nPutBombCount =0;
+};
+
