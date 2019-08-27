ad5e6fd2beb6f3e7772c372cac565b15e9867c14
CBueong <54173797+CBueong@users.noreply.github.com>
2019-08-28 01:02:12 +0900

    clean up

--- compare practice/FPS2_ori/SceneFactory.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class SceneFactory
-{
-public:
-	static class Scene* Make(class SceneManager* a_pParent, eScene a_eScene);
-};
--- compare practice/FPS7/FPS7_HJH/ObjectFactory.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class ObjectFactory
-{
-public:
-	static class Object* Make(eObjectType a_eType, int x, int y);
-};
